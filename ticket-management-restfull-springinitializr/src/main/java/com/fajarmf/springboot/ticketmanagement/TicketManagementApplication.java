f66ed4555fb826da5e7ecf63ac1a68a46144983d
Fajar Maulana Firdaus <fajarmf78@gmail.com>
2019-04-17 14:14:20 +0700

    RESTful

--- ticket-management-restfull-springinitializr/src/main/java/com/fajarmf/springboot/ticketmanagement/TicketManagementApplication.java
+++ ticket-management-restfull-springinitializr/src/main/java/com/fajarmf/springboot/ticketmanagement/TicketManagementApplication.java
@@ -2,7 +2,9 @@ package com.fajarmf.springboot.ticketmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.fajarmf")
 @SpringBootApplication
 public class TicketManagementApplication {
 
